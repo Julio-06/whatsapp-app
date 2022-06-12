8e4c52a5b01ee346fc78552551fb84586083d121
Julio-06 <jtcesarin1998@gmail.com>
2022-06-12 12:54:03 -0500

    [ADD] migration and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,7 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Julio Tejeira',
+            'email' => 'jtcesarin1998@gmail.com',
+            'password' => bcrypt('12345678')
+        ]);
+
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
